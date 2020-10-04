ee43914a16d43207ba50768387e35320314b707e
Felix Sjödin <71407281+PolynomialDuck@users.noreply.github.com>
2020-10-04 02:28:54 +0200

    Mandatory Update for Working Version

--- KomplexTal.scala
+++ KomplexTal.scala
@@ -1,29 +1,25 @@
 package MandelbrotSet
 
 class Complex(var re: Double, var im: Double){
-    def +(c: Complex): Unit = { 
+    def +(c: Complex): Complex = { 
         val tempRe = re 
         val tempIm = im
-        re=(tempRe+c.re) 
-        im=(tempIm+c.im)
+        new Complex((tempRe+c.re),(tempIm+c.im))
     }
-    def -(c: Complex): Unit = {
+    def -(c: Complex): Complex = {
         val tempRe = re 
         val tempIm = im 
-        re=(tempRe-c.re) 
-        im=(tempIm-c.im)
+        new Complex ((tempRe-c.re),(tempIm-c.im))
     }
-    def *(c: Complex): Unit = {
+    def *(c: Complex): Complex = {
         val tempRe = re 
         val tempIm = im
-        re=(tempRe*c.re-tempIm*c.im)
-        im=(tempRe*c.im+tempIm*c.re)
+        new Complex((tempRe*c.re-tempIm*c.im),(tempRe*c.im+tempIm*c.re))
     }
-    def /(c: Complex): Unit = {
+    def /(c: Complex): Complex = {
         val tempRe = re 
         val tempIm = im
-        re=((tempRe*c.re+tempIm*c.im)/(c.re*c.re+c.im*c.im))
-        im=((tempIm*c.re-tempRe*c.im)/(c.re*c.re+c.im*c.im))
+        new Complex(((tempRe*c.re+tempIm*c.im)/(c.re*c.re+c.im*c.im)),((tempIm*c.re-tempRe*c.im)/(c.re*c.re+c.im*c.im)))
     }
     def magni: Int = (scala.math.sqrt((re*re)+(im*im))).toInt
     def theta = scala.math.atan2(re, im)
