8402411f4fa1a6b2996f670b3d24d7908071d548
Felix Sjödin <71407281+PolynomialDuck@users.noreply.github.com>
2020-10-12 17:59:35 +0200

    Rate of Divergence

--- ComplexPlane.scala
+++ ComplexPlane.scala
@@ -2,16 +2,18 @@ package MandelbrotSet
 
 import introprog.PixelWindow
 import java.awt.{Color => JColor}
+import java.util.concurrent.CompletionException
 
 object Color{
-    var setColor = new JColor(0,0,0)
-    var backColor = new JColor(0,0,255)
+    var setTupel = new Tupel(0,0,0)
+    var backTupel = new Tupel(0,0,255)
+    var divTupel = new Tupel(255,0,0)
+    var divColor = new JColor(0,0,0)
 }
 
-
 object Window{
-    val windowSize = (510,510)
-    val window = new PixelWindow(windowSize._1,windowSize._2, "Mandelbrot", Color.backColor)
+    val windowSize = (501,501)
+    val window = new PixelWindow(windowSize._1,windowSize._2, "Mandelbrot", Color.backTupel.jColor())
     def complexValue(p: (Int, Int)): Complex = {
     val x: Double = p._1.toDouble
     val y: Double = p._2.toDouble
@@ -42,27 +44,43 @@ object Math{
     var ConstantOriented: Boolean = true
     var cTemp: Complex = new Complex(0,0)
     var constant: Complex = new Complex(0,0)
+    var divType: Double = 0.0
+    var divWeight: Double = 0.1
+    
     def checkRekursivFormel(c: Complex): Boolean = {
         if(ConstantOriented){
+            divType = 0.0
             cTemp = new Complex(0,0)
             for(i <- 1 to 1000){
-                //println(cTemp.magni)
                 cTemp=(cTemp*cTemp)+c
+                if(i<10){
+                divType+= cTemp.magni
+                }
             }
             if (cTemp.magni<2){
                 return true
             }
-            else return false
+            else{
+                Color.divColor = ((Color.backTupel*(divType*divWeight,divType*divWeight,divType*divWeight)).addTupel(Color.divTupel*(1/(divType*divWeight), 1/(divType*divWeight), 1/(divType*divWeight)))).jColor()
+                return false
+            }
         }
         else{
+            divType = 0.0
             cTemp = c
             for(i <- 1 to 1000){
                 cTemp=cTemp*cTemp+constant
+                if(i<10){
+                    divType+= cTemp.magni
+                }
             }
             if (cTemp.magni<2){
                 return true
             }
-            else return false
+            else {
+                Color.divColor = ((Color.backTupel*(divType*divWeight,divType*divWeight,divType*divWeight)).addTupel(Color.divTupel*(1/(divType*divWeight), 1/(divType*divWeight), 1/(divType*divWeight)))).jColor()
+                return false
+            }
         }
     }
 }
@@ -72,16 +90,21 @@ object Main {
         for(y <- 0 to 500){
             for(x <- 0 to 500){
                 if(Math.checkRekursivFormel(Window.complexValue(x,y))){
-                    Window.window.setPixel(x,y,Color.setColor)
+                    Window.window.setPixel(x,y,Color.setTupel.jColor())
                     Thread.sleep(0)
+                    }
+                    else{
+                        if(Color.divColor != new JColor(0,0,0)){
+                            Window.window.setPixel(x,y,Color.divColor)
+                        }
+                        else{
+                            Window.window.setPixel(x,y,Color.backTupel.jColor())
+                        }
                     }
                 }
             }
         }
     def main(args: Array[String]): Unit = {
-        var tempR: Int = 0
-        var tempG: Int = 0
-        var tempB: Int = 0
         var tempAns: Int = 0
         var tempRe: Double = 0.0
         var tempIm: Double = 0.0
@@ -89,19 +112,19 @@ object Main {
         var quit = false
         while(!quit){
             try{
-               tempAns=Window.makeChoice("Pick a choice: ", Vector("Change to Mandelbrotset", "Change to Juliaset", "Change Color of Background", "Change Color of Set","Quit"))
+               tempAns=Window.makeChoice("Pick a choice: ", Vector("Change to Mandelbrotset", "Change to Juliaset", "Change Color of Background", "Change Color of Set","Change the weight of divergence", "Quit"))
             } catch {
                 case e: Exception => println("Error: Couldn't understand the input, try again.")
                 tempAns=(-1)
             }
-            if(0<=tempAns && tempAns<=4){
+            if(0<=tempAns && tempAns<=5){
                 if(tempAns==0){
                     if(Math.ConstantOriented==true){
                         println("You are already viewing the MandelbrotSet!")
                     }
                     else{
                     Math.ConstantOriented = true
-                    Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backColor)
+                    Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backTupel.jColor())
                     drawMandelbrot()
                     }
                 }
@@ -121,58 +144,66 @@ object Main {
                             }
                             Math.constant=new Complex(tempRe,tempIm)
                             Math.ConstantOriented = false
-                            Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backColor)
+                            Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backTupel.jColor())
                             drawMandelbrot()
                 }
                 else if(tempAns==2){
                         println("Choose a color using the RGB system...")
                         try{
-                        tempR = Window.valColor(io.StdIn.readLine("Red = ").toInt)
+                        Color.backTupel.r = Window.valColor(io.StdIn.readLine("Red = ").toInt)
                         } catch{ 
                                 case e: Exception => println("Error: Couldn't understand the input. Returning value 0...")
-                                tempR=0
+                                Color.backTupel.r=0
                         }
                         try{
-                        tempG = Window.valColor(io.StdIn.readLine("Green = ").toInt)
+                        Color.backTupel.g = Window.valColor(io.StdIn.readLine("Green = ").toInt)
                         } catch{ 
                                 case e: Exception => println("Error: Couldn't understand the input. Returning value 0...")
-                                tempG=0
+                                Color.backTupel.g=0
                         }
                         try{
-                        tempB = Window.valColor(io.StdIn.readLine("Blue = ").toInt)
+                        Color.backTupel.b = Window.valColor(io.StdIn.readLine("Blue = ").toInt)
                         } catch{ 
                                 case e: Exception => println("Error: Couldn't understand the input. Returning value 0...")
-                                tempB=0
+                                Color.backTupel.b=0
                         }
-                        Color.backColor = new JColor(tempR, tempG, tempB)
-                        Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backColor)
+                        Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backTupel.jColor())
                         drawMandelbrot()
                 }
                 else if(tempAns==3){
                         println("Choose a color using the RGB system...")
                         try{
-                        tempR = Window.valColor(io.StdIn.readLine("Red = ").toInt)
+                        Color.setTupel.r = Window.valColor(io.StdIn.readLine("Red = ").toInt)
                         } catch{ 
                                 case e: Exception => println("Error: Couldn't understand the input. Returning value 0...")
-                                tempR=0
+                                Color.setTupel.r=0
                         }
                         try{
-                        tempG = Window.valColor(io.StdIn.readLine("Green = ").toInt)
+                        Color.setTupel.g = Window.valColor(io.StdIn.readLine("Green = ").toInt)
                         } catch{ 
                                 case e: Exception => println("Error: Couldn't understand the input. Returning value 0...")
-                                tempG=0
+                                Color.setTupel.g=0
                         }
                         try{
-                        tempB = Window.valColor(io.StdIn.readLine("Blue = ").toInt)
+                        Color.setTupel.b = Window.valColor(io.StdIn.readLine("Blue = ").toInt)
                         } catch{ 
                                 case e: Exception => println("Error: Couldn't understand the input. Returning value 0...")
-                                tempB=0
+                                Color.setTupel.b=0
                         }
-                        Color.setColor = new JColor(tempR, tempG, tempB)
-                        Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backColor)
+                        Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backTupel.jColor())
                         drawMandelbrot()
                     }
                 else if(tempAns==4){
+                    try{
+                        Math.divWeight = io.StdIn.readLine("Divergence Weight = ").toDouble
+                    } catch{
+                        case e: Exception => println("Error: Couldn't understand the input. Returning value 0.5...")
+                        Math.divWeight = 0.5
+                    }
+                    Window.window.fill(0,0,Window.windowSize._1,Window.windowSize._2, Color.backTupel.jColor())
+                    drawMandelbrot()
+                }
+                else if(tempAns==5){
                     quit = true
                     Window.window.hide()
                 }
